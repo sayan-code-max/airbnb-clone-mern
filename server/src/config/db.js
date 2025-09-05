d26d909db61872aeec2a7b8331cefa346d692865
Sayan Mandal <sm8304098@gmail.com>
2025-09-06 04:04:43 +0530

    Use MongoDB Atlas connection string

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,9 +1,23 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   const uri = process.env.MONGO_URI;
+//   if (!uri) throw new Error("MONGO_URI not set");
+//   mongoose.set("strictQuery", true);
+//   await mongoose.connect(uri);
+//   console.log("Connected to MongoDB");
+// };
+
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  const uri = process.env.MONGO_URI;
-  if (!uri) throw new Error("MONGO_URI not set");
-  mongoose.set("strictQuery", true);
-  await mongoose.connect(uri);
-  console.log("Connected to MongoDB");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("DB connection error:", err);
+    process.exit(1);
+  }
 };
+
+export default connectDB;
