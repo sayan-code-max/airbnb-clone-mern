3d202d25ffc26bcd449f766e20c20ad78a815bda
Sayan Mandal <sm8304098@gmail.com>
2025-09-06 15:57:24 +0530

    update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://airbnb-clone-mern-xh2q.onrender.com/api',
 })
 
 // attach token if present
