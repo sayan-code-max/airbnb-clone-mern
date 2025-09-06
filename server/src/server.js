00c3f8ef75401d9cbf868f3384cf723373aa32d2
Sayan Mandal <sm8304098@gmail.com>
2025-09-06 16:14:39 +0530

    bruhh

--- server/src/server.js
+++ server/src/server.js
@@ -16,7 +16,10 @@ app.use(morgan("dev"));
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || "https://airbnb-clone-mern-5fnu.vercel.app",
+  origin: [
+    "https://airbnb-clone-mern-p5ks.vercel.app",
+    "http://localhost:5173"
+  ],
   credentials: true
 }));
 
